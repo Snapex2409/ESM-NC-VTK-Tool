44c16555243bf6afe08f35c672e93e57af76a761
Alex Hocks <alex.hocks@tum.de>
2025-08-27 12:41:53 +0200

    repo fix
    nothing real happened here

--- scripts/vars.sh
+++ scripts/vars.sh
@@ -14,21 +14,21 @@ NC_MESH="${NC_DIR}/bench_grids.nc"
 NC_MASK="${NC_DIR}/bench_masks_ocea_not_atm.nc"
 
 # directories for data of each step
-RES="TH0_001"
+RES="TH0_2"
 DS1="${OUT}/01_corner_meshes"
-DS2="${OUT}/02_masks"
-DS3="${OUT}/03_center_meshes/$RES"
-DS4="${OUT}/04_evaluated_meshes/$RES"
-DS5="${OUT}/05_mapped_meshes/$RES"
-DS6="${OUT}/06_metrics/$RES"
+DS2="${OUT}/02_masks/${RES}"
+DS3="${OUT}/03_center_meshes/${RES}"
+DS4="${OUT}/04_evaluated_meshes/${RES}"
+DS5="${OUT}/05_mapped_meshes/${RES}"
+DS6="${OUT}/06_metrics/${RES}"
 
 
 SEA_VARS=("nogt" "torc")
-ATM_VARS=("bggd" "icoh" "icos" "sse7")
+ATM_VARS=("bggd" "icos" "sse7") # excluding icoh for faster computation
 FUNCTIONS=("sinusoid" "harmonic" "vortex" "gulfstream")
 MAPPINGS=("nn" "np" "rbf")
 
-THRESHOLD=0.001
+THRESHOLD=0.2
 BAD_TORC=0
 
 cd $OPWD
